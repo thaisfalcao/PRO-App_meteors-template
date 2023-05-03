0abfb7abc328c39fe72ccfad421d08f36e20b46b
Thaís Falcão <101199332+thaisfalcao@users.noreply.github.com>
2023-05-03 14:31:17 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
 import HomeScreen from './screens/Home';
 import IssLocationScreen from './screens/IssLocation';
 import MeteorsScreen from './screens/Meteors';
 
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-const Stack = createStackNavigator();
-
-function App(){
-  return(
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home' screenOptions={{headerShown: false}}>
-        <Stack.Screen name='Home' component={HomeScreen}/>
-        <Stack.Screen name='IssLocation' component={IssLocationScreen}/>
-        <Stack.Screen name='Meteors' component={MeteorsScreen}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MeteorsScreen/>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
